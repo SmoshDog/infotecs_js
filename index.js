a44cbaac8041b6333ad7cf2c099bbaab65240b8a
SmoshDog <daniil.alenkin@yandex.ru>
2023-01-19 17:42:13 +0300

    make code secure

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ import { customers } from "./customers.js";
 
 const customersTableTbody = document.querySelector("#customers-table tbody");
 
-customers.forEach((customer) => {
-  customersTableTbody.innerHTML += `
-    <tr>
-      <td>${customer.name.firstName}</td>
-      <td>${customer.name.lastName}</td>
-      <td>${customer.about}</td>
-      <td>${customer.eyeColor}</td>
-    </tr>
-  `;
-});
+const clients = customers.map((client) => ({
+  firstName: client.name.firstName,
+  lastName: client.name.lastName,
+  about: client.about,
+  eyeColor: client.eyeColor,
+}));
+// console.log(clients);
+
+const tr = document.createElement("tr");
+const td = document.createElement("td");
+const keysOfClientObject = Object.keys(clients[0]);
+
+// console.log(keysOfClientObject);
+
+for (let i = 0; i < clients.length; i++) {
+  const trCloned = tr.cloneNode(false);
+  for (let j = 0; j < keysOfClientObject.length; j++) {
+    const tdCloned = td.cloneNode(false);
+    const cellValue = clients[i][keysOfClientObject[j]];
+    // console.log(clients[i]);
+    tdCloned.appendChild(document.createTextNode(cellValue || ""));
+    trCloned.appendChild(tdCloned);
+  }
+
+  customersTableTbody.appendChild(trCloned);
+}
 
 // https://stackoverflow.com/a/21065846
